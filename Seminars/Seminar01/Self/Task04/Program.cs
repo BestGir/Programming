1d47d70838c6387c3c79302414b112a8e03fb0bd
sannikog <sannikog@gmail.com>
2023-09-14 23:57:04 +0300

    Make Task04

--- Seminars/Seminar01/Self/Task04/Program.cs
+++ Seminars/Seminar01/Self/Task04/Program.cs
@@ -1,1 +1,21 @@
-﻿// Your code here!
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        //Console's color
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        //Reading the name
+        string surname = Console.ReadLine();
+        string name = Console.ReadLine();
+        string middleName = Console.ReadLine();
+        //Data output 
+        Console.WriteLine($"Здравствуйте, {surname} {name} {middleName}!");
+        //Restoring console values
+        Console.ResetColor();
+
+    }
+}
