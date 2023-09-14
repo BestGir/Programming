e561db98b35e49cbcd98eab71a5c222e6194ed2c
sannikog <sannikog@gmail.com>
2023-09-14 23:57:43 +0300

    Make Task05

--- Seminars/Seminar01/Self/Task05/Program.cs
+++ Seminars/Seminar01/Self/Task05/Program.cs
@@ -1,1 +1,12 @@
-﻿// Your code here!
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        //Reading data
+        double U = double.Parse(Console.ReadLine());
+        double R = double.Parse(Console.ReadLine());
+        //Data output 
+        Console.WriteLine($"I = {(U/R)} \nP = {U*U/R}");
+    }
+}
