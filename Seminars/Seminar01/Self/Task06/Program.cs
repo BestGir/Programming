30afedcb1bb2c8220205e9440b97856699968107
sannikog <sannikog@gmail.com>
2023-09-14 23:58:25 +0300

    Make Task06

--- Seminars/Seminar01/Self/Task06/Program.cs
+++ Seminars/Seminar01/Self/Task06/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //Reading data
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        //Data output 
+        Console.WriteLine($"Гипотенуза равна {Math.Sqrt(a*a+b*b)}");
+    }
+}
